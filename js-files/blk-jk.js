a2ab31f523ef161e78c26d2e7be84e5b390286c4
GioMonci <alexmoncibaez@yahoo.com>
2024-07-20 15:46:34 -0400

    added proper names to cards suits

--- js-files/blk-jk.js
+++ js-files/blk-jk.js
@@ -1,6 +1,6 @@
 
 let gameState = {
-    suit: ["Sigma ", "Alpha ", "Jittleyang ", "Futuluhtoogan "],
+    suit: ["Clubs ", "Diamonds ", "Hearts ", "Spades "],
     rank: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
     playersMoney: 10,
     dealersTotal: 0,
